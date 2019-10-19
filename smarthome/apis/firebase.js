26c8c7675c4467719e5a087727d936041c80a8b4
TatagW <tatagwibowo95@gmail.com>
2019-10-19 16:51:58 +0700

    delete other

--- smarthome/apis/firebase.js
+++ smarthome/apis/firebase.js
@@ -3,4 +3,5 @@ import { db } from '../configs/firebase'
 export const User = db.collection('doors')
 export const Lamp = db.collection('lamps')
 export const Room = db.collection('rooms')
-export const Sensor = db.collection('sensor')
+export const Sensor = db.collection('sensors')
+export const Scheduler = db.collection('schedulers')
--- smarthome/screens/scheduler/days.js
+++ smarthome/screens/scheduler/days.js
@@ -1,44 +1,101 @@
-import React, {useState} from 'react'
-import {View, Text, StatusBar} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, Text, StatusBar, ActivityIndicator, Dimensions, TouchableOpacity} from 'react-native'
 import Constants from 'expo-constants';
-import Device from './device'
+import {Lamp} from '../../apis/firebase'
+import ToggleSwitch from 'toggle-switch-react-native'
+const screenWidth = Math.round(Dimensions.get('window').width);
 
 
 export default (props) => {
-    const [on, setON] = useState(false)
+    const [lamps, setLamps] = useState([])
+    const [loading, setLoading] = useState(true)
+    let unsubscribe = null
 
-    const data1 = {
-        status: on,
-        name: 'lamp1',
-    }
-    const data2 = {
-        status: on,
-        name: 'lamp2',
-    }
-    const data3 = {
-        status: on,
-        name: 'door',
-    }
-    const data4 = {
-        status: on,
-        name: 'heater',
+    const fetchLamp = () => {
+        unsubscribe = Lamp.where("userId", "==", "123")
+        .onSnapshot(function(querySnapshot) {
+            const newLamps = []
+            querySnapshot.forEach(function(doc) {
+                newLamps.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setLamps(newLamps)
+            setLoading(false)
+        });
     }
 
-    const toggleDevice = (status) => {
-        setON(status)
+    const changeDay = (id, val) => { 
+        Lamp.doc(id).update({day: val})
+        .then(function() {
+            console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+    } 
+    const changeDayAuto = (id, val) => {
+        console.log(id, val,'<<<<<<<<<<')
+        Lamp.doc(id).update({ dayAuto: val })
+        .then(function() {
+            console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
     }
+    useEffect(() => {
+        return () => {
+          unsubscribe()
+        };
+      }, []);
+    useEffect(() => {
+        fetchLamp()
+    },[])
     return (
         <>
             <StatusBar barStyle={'dark-content'} />
             <View style={{flex: 1, backgroundColor: '#f9f9f9'}}>
                 <View style={{marginTop: Constants.statusBarHeight, flex: 1, alignItems: 'center', width: '100%'}}>
-                    <Device data={data1} toggle={toggleDevice} />
-                    <Device data={data2} toggle={toggleDevice} />
-                    <Device data={data3} toggle={toggleDevice} />
-                    <Device data={data4} toggle={toggleDevice} />
-                    <View style={{padding: 30}}>
-                        <Text style={{color: 'silver', textAlign: 'center'}}>When the sun rises. All the selected devices will be turned on</Text>
-                    </View>
+                    {
+                        loading ? <ActivityIndicator size="small" color="#e8d296"/> :
+                        <>
+                            {
+                                lamps.map(lamp => {
+                                    let shadow = null
+                                    let labelAuto = ""
+                                    lamp.dayAuto ? labelAuto = "Auto on" : labelAuto = "Auto off"
+                                    lamp.status ? shadow = "#c18501" : shadow = '#000'
+                                    
+                                    return (
+                                        <TouchableOpacity key={lamp.id} onPress={() => {
+                                                changeDayAuto(lamp.id, !lamp.dayAuto)
+                                            }
+                                        }>
+                                            <View  style={[styles.menu, {shadowColor: shadow}]}>
+                                                <Text style={styles.text}>{lamp.name}</Text>
+                                                <ToggleSwitch
+                                                    label={labelAuto}
+                                                    isOn={lamp.day}
+                                                    labelStyle={{color: 'silver'}}
+                                                    onColor="#e8d296"
+                                                    offColor="#dbdbdb"
+                                                    size="medium"
+                                                    onToggle={isOn => changeDay(lamp.id, isOn)}
+                                                />
+                                            </View>
+                                        </TouchableOpacity>
+                                    )
+                                })
+                            }
+                            <View style={{padding: 30}}>
+                                <Text style={{color: 'silver', textAlign: 'center'}}>When the sun rises. All the selected devices will be turned on or off</Text>
+                            </View>
+                        </>
+                    }                   
                 </View>
             </View>
         </>
@@ -46,4 +103,31 @@ export default (props) => {
 }
 
 
-  
+const styles = {
+    menu: {
+        width: screenWidth*0.9,
+        height: 80,
+        borderRadius: 15,
+        backgroundColor: 'white',
+        marginBottom: 20,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+    text: {
+      fontSize: 22,
+      fontWeight: '600',
+      letterSpacing: 2,
+      color: '#383838'
+    }
+}
