16baac7701d1b8dad265138f3242da63db97127b
TatagW <tatagwibowo95@gmail.com>
2019-10-20 16:37:58 +0700

    handle auto

--- smarthome/screens/detail/item/index.js
+++ smarthome/screens/detail/item/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import ToggleSwitch from 'toggle-switch-react-native'
 import { Lamp } from '../../../apis/firebase'
+import {Alert} from 'react-native'
 
 export default (props) => {
   const { item, navigation } = props
@@ -9,13 +10,36 @@ export default (props) => {
   const [isOn, setIsOn] = useState(item.status)
 
   const toggleHandler = async () => {
-    setIsOn(!isOn)
-
-    try {
-      await Lamp.doc(item.id).update({ status: !isOn })
-    } catch (err) {
-      console.log(err)
-    }
+    if(item.day || item.night){
+      Alert.alert(
+        'This lamp is using light sensor',
+        'Clicking ok will turn them off',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          {text: 'OK', onPress: async () => {
+            try {
+              await Lamp.doc(item.id).update({ day: false, night: false, status: !isOn })
+              setIsOn(!isOn)
+            } catch (err) {
+              console.log(err)
+            }
+          }},
+        ],
+        {cancelable: false},
+        );
+      }
+      else{
+        setIsOn(!isOn)
+        try {
+          await Lamp.doc(item.id).update({ status: !isOn })
+        } catch (err) {
+          console.log(err)
+        }
+      }
   }
 
   const modalHandler = () => {
