3d305a6ec39c64992013214a96b947f3eadd2def
TatagW <tatagwibowo95@gmail.com>
2019-10-21 10:09:52 +0700

    done

--- smarthome/screens/scheduler/nights.js
+++ smarthome/screens/scheduler/nights.js
@@ -5,10 +5,14 @@ import {Lamp} from '../../apis/firebase'
 import ToggleSwitch from 'toggle-switch-react-native'
 import {db} from '../../configs/firebase'
 import * as Font from 'expo-font';
+import { Feather } from '@expo/vector-icons';
+
 const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 
 export default (props) => {
+    const modal = props.modal
     const [fontLoaded, setFont] = useState(false)
     const [lamps, setLamps] = useState([])
     const [loading, setLoading] = useState(true)
@@ -68,9 +72,9 @@ export default (props) => {
     },[])
     return (
         <>
-            <StatusBar barStyle={'dark-content'} />
+            <StatusBar barStyle={'light-content'} />
             <View style={{flex: 1, backgroundColor: 'black'}}>
-                <View style={{marginTop: Constants.statusBarHeight, flex: 1, alignItems: 'center', width: '100%'}}>
+                <View style={{marginTop: Constants.statusBarHeight + (screenHeight*0.07), flex: 1, alignItems: 'center', width: '100%'}}>
                     {
                         loading ? <ActivityIndicator size="small" color="#e8d296"/> :
                         <>
@@ -106,7 +110,28 @@ export default (props) => {
                                 <Text style={{color: 'silver', textAlign: 'center'}}>When the sun sets. All the selected devices will be turned on or off</Text>
                             </View>
                         </>
-                    }                   
+                    }    
+                    <TouchableOpacity style={{
+                        position: "absolute",
+                        bottom: 80,}} 
+                        onPress={()=>modal(false)}>
+                        <View style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width:60, height: 60, 
+                            borderRadius: '100', 
+                            backgroundColor: 'white',
+                            shadowColor: "#000",
+                            shadowOffset: {
+                                width: 0,
+                                height: 2,
+                            },
+                            shadowOpacity: 0.25,
+                            shadowRadius: 3.84,
+                            elevation: 5,}}>
+                            <Feather name="x" size={30} color="#383838" />
+                        </View>
+                    </TouchableOpacity>                
                 </View>
             </View>
         </>
